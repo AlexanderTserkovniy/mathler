6b90df875dc6972385c086116e0e2ef2aed16657
Oleksandr Tserkovnyi <kemperomg@gmail.com>
2022-08-04 23:03:49 -0400

    docs: add examples for regexps

--- src/common/constants.js
+++ src/common/constants.js
@@ -8,8 +8,10 @@
 export const MINUS_WRAPPER = "minus-minus";
 export const MINUS_WRAPPER_REG_EXP = "minus(-)minus";
 // captures minus to the group $2
+// see examples here https://regexr.com/6r8um
 export const MINUS_REG_EXP = /(-?\d+)(-)(?=-?\d+)/g;
 // using constructor in order to put minus wrapper group as interpolation
+// see examples here https://regexr.com/6r92t
 export const EQUATION_REG_EXP = new RegExp(
   `-?\\d+|[/*+]|${MINUS_WRAPPER_REG_EXP}`,
   "g"
