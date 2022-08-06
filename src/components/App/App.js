51475831123ce0e9cca8f856c95afbc353f0712b
Oleksandr Tserkovnyi <kemperomg@gmail.com>
2022-08-05 23:52:20 -0400

    feat: run more examples in react, add more equations to json

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,6 +14,12 @@ function App() {
         <code>10/2+9</code>
         <p>result:</p>
         <code>{calculate("10/2+9")}</code>
+        <br />
+        <code>{calculate("90*5-1")}</code>
+        <br />
+        <code>{calculate("-9*2/1")}</code>
+        <br />
+        <code>{calculate("-3+2/8")}</code>
       </article>
     </div>
   );
